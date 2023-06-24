49fe6b72c452d66ea2b0f1b9d07a1b54d0850195
Mikyas Kebede <mikyasyesigat@gmail.com>
2023-06-23 18:42:56 -0700

    backend commit

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -4,25 +4,20 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 const morgan=require("morgan")
+const db=require('./data/db.json')
  
 app.use(cors());
 app.use(express.json());
 //app.use("/", userRouter);
 
+
 app.use(morgan("tiny"))
 
+app.get('/',(req,res)=>{
+res.status(200)
+res.send(db)
 
-
-
-
-
-
-
-
-
-
-
-
+})
 
 module.exports=app
 
